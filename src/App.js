e9991ba6c88b3c2ed59e28826e6abfc5de33ec5e
Jason-Williams-github <jwilliams@talentpath.com>
2021-05-27 16:12:35 -0400

    fixed singlelocation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import logo from './logo.svg';
 import { Switch, useParams } from 'react-router-dom'
 import './App.css';
-import SingleLocation from './SingleLocation/SingleLocation'
-import getAllLocations from './Service/fileService'
 import {BrowserRouter as Router, Route } from 'react-router-dom'
 import React, { useState, useEffect } from 'react';
 import {LandingPage} from "./LandingPage/LandingPage"
