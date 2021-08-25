c91dea29674d122f2676b4d491cbb41305d95d97
hyltonw <57882834+hyltonw@users.noreply.github.com>
2021-08-25 10:20:52 -0400

    Update fileService.js

--- src/Service/fileService.js
+++ src/Service/fileService.js
@@ -8,7 +8,7 @@ import axios from "axios";
 
 export async function getAllLocations(url) {
   const locations = await axios.get(
-    `http://bcmapplication-env.eba-yuipzc2r.us-east-2.elasticbeanstalk.com/api/files/${url}`
+    `https://cors-everywhere-me.herokuapp.com/http://bcmapplication-env.eba-yuipzc2r.us-east-2.elasticbeanstalk.com/api/files/${url}`
     // `https://bcm-project.bcmfilelverification.com/api/files/${url}`
     // `http://localhost:8080/api/files/${url}`
   );
@@ -20,19 +20,19 @@ export function submitFiles(ReviewedFile) {
   axios.put(
     // `https://bcm-project.bcmfilelverification.com/api/files/response`,
     // ReviewedFile
-    `http://bcmapplication-env.eba-yuipzc2r.us-east-2.elasticbeanstalk.com/api/files/response`,
+    `https://cors-everywhere-me.herokuapp.com/http://bcmapplication-env.eba-yuipzc2r.us-east-2.elasticbeanstalk.com/api/files/response`,
     ReviewedFile
   );
   // axios.put(`http://localhost:8080/api/files/response`, ReviewedFile);
   window.location.replace(
-    `https://main.d31lfvg6uu6z53.amplifyapp.com/submitted`
+    `https://cors-everywhere-me.herokuapp.com/https://main.d31lfvg6uu6z53.amplifyapp.com/submitted`
   );
 }
 
 export async function getAllUsers() {
   const users = await axios.get(
     // `https://bcm-project.bcmfilelverification.com/api/files/allUsers`
-    `http://bcmapplication-env.eba-yuipzc2r.us-east-2.elasticbeanstalk.com/api/files/allUsers`
+    `https://cors-everywhere-me.herokuapp.com/http://bcmapplication-env.eba-yuipzc2r.us-east-2.elasticbeanstalk.com/api/files/allUsers`
   );
   // const users = await axios.get(`http://localhost:8080/api/files/allUsers`);
   const data = users.data;
@@ -43,7 +43,7 @@ export async function transferFiles(newOwner, oldOwner) {
   axios.put(
     // `https://bcm-project.bcmfilelverification.com/api/files/${oldOwner}`,
     // newOwner
-    `http://bcmapplication-env.eba-yuipzc2r.us-east-2.elasticbeanstalk.com/api/files/${oldOwner}`,
+    `https://cors-everywhere-me.herokuapp.com/http://bcmapplication-env.eba-yuipzc2r.us-east-2.elasticbeanstalk.com/api/files/${oldOwner}`,
     newOwner
   );
   // axios.put(`http://localhost:8080/api/files/${oldOwner}`, newOwner);
@@ -52,7 +52,7 @@ export async function transferFiles(newOwner, oldOwner) {
 export function deleteFiles(url) {
   axios.delete(
     // `https://bcm-project.bcmfilelverification.com/api/files/${url}`
-    `http://bcmapplication-env.eba-yuipzc2r.us-east-2.elasticbeanstalk.com/api/files/${url}`
+    `https://cors-everywhere-me.herokuapp.com/http://bcmapplication-env.eba-yuipzc2r.us-east-2.elasticbeanstalk.com/api/files/${url}`
   );
 
   // axios.delete(`http://localhost:8080/api/files/${url}`);
@@ -62,7 +62,7 @@ export function addTransferLog(transferLog) {
   axios.post(
     // `https://bcm-project.bcmfilelverification.com/api/files/transfer`,
     // transferLog
-    `http://bcmapplication-env.eba-yuipzc2r.us-east-2.elasticbeanstalk.com/api/files/transfer`,
+    `https://cors-everywhere-me.herokuapp.com/http://bcmapplication-env.eba-yuipzc2r.us-east-2.elasticbeanstalk.com/api/files/transfer`,
     transferLog
   );
   // axios.post(`http://localhost:8080/api/files/transfer`, transferLog);
